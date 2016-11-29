d09d0948e1980c1ac14b9556a332dc2ea8fd261b
cristian <cristian.fonolla@gmail.com>
2016-11-29 21:52:32 +0100

    task controller test funcionant correctament amb mocks

--- tests/TaskControllerTest.php
+++ tests/TaskControllerTest.php
@@ -13,6 +13,22 @@ class TaskControllerTest extends TestCase
 
     use DatabaseMigrations;
 
+    protected $apiResult = '[{
+"name": "Suscipit qui vitae voluptatem illo unde neque commodi.",
+"done": true,
+"priority": 5
+},
+{
+    "name": "Quia et dolores et.",
+"done": true,
+"priority": 8
+},
+{
+    "name": "Quaerat dicta aperiam unde dicta ut repellendus excepturi necessitatibus.",
+"done": true,
+"priority": 5
+}]';
+
     /**
      * TaskControllerTest constructor.
      */
@@ -53,24 +69,24 @@ class TaskControllerTest extends TestCase
      */
     public function testExample()
     {
-        //Prepare
-        //Execute
-        //Comprovaci/Assercions
+        $stream = GuzzleHttp\Psr7\stream_for('{"data" : '. $this->apiResult . ' }');
+        $response = new \GuzzleHttp\Psr7\Response(
+            200,
+            ['Content-Type' => 'application/json'],
+            $stream
+        );
+        //1 Prepare test
         $this->login();
-
+        //1.1 Isolate
         $this->mock
-             ->shouldReceive('request')
-             ->once()
-             ->andReturn('foo');
-
-        $this->app->instance('GuzzleHttp\Client',$this->mock);
-
-
-
+            ->shouldReceive('request')
+            ->once()
+            ->andReturn($response);
+        $this->app->instance('GuzzleHttp\Client', $this->mock);
+        //2 Execute test
         $response = $this->call('GET', '/tasks');
-
+        //3 comprovacions/assercions/shoulds/expectations
         $this->assertEquals(200, $response->status());
-
         $this->assertViewHas('tasks');
 
 
